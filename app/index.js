7b4605fe6646a317722e81d6081f2cdf70e85fd3
Lorin Lochridge <lorin.lochridge@matcotools.com>
2018-04-12 11:22:50 -0400

    Entire project

---
+++ app/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { Provider } from 'react-redux';
+import Navigator from './config/routes';
+import { AlertProvider } from './components/Alert';
+import store from './config/store';
+
+EStyleSheet.build({
+  $primaryBlue:'#4F6d7A',
+  $primaryOrange:'#D57a54',
+  $primaryGreen:'#00bd9d',
+  $primaryPurple:'#9e768f',
+
+  $white: '#ffffff',
+  $border: '#e2e2e2',
+  $inputText: '#797979',
+  $lightGray: '#f0f0f0',
+  $darkText: '#343434',
+});
+
+export default () => (
+  <Provider store={store}>
+    <AlertProvider>
+      <Navigator onNavigationStateChange={null} />
+    </AlertProvider>
+  </Provider>
+);
